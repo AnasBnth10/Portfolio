670a3a7eb769e93b992e778d7525d5b8870a4520
AnasBnth10 <anasbnth@gmail.com>
2023-04-16 20:37:12 +0200

    Changed api endpoint

--- API/Program.cs
+++ API/Program.cs
@@ -41,7 +41,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy(name: MyAllowSpecificOrigins,
                       policy =>
                       {
-                          policy.WithOrigins("http://localhost:3000", "https://bentaheranas.azurewebsites.net/");
+                          policy.WithOrigins("http://localhost:3000", "https://anasbentaher.azurewebsites.net/");
                       });
 });
 
